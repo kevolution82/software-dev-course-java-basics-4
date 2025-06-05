483464f14172fa99820fea3bcb1d4ad772bb4264
kevdude82 <altvbusiness@gmail.com>
2025-06-05 09:26:15 -0500

    Finished assignment

--- src/main/java/org/example/CollectionChoiceExercises.java
+++ src/main/java/org/example/CollectionChoiceExercises.java
@@ -12,13 +12,13 @@ public class CollectionChoiceExercises {
         // Create an appropriate variable for holding a collection
         // of three strings that are known ahead of time to be "Red", "Green" and "Blue"
         // and will not change during program execution.
-
         String[] colors;
     }
 
     public void MakeStudentCollection() {
         // Create an appropriate collection variable for holding a list of student names
         // that will be added to and removed from during program execution.
+        ArrayList<String> studentNames;
     }
 
     public void MakeLoggedInUsersCollection() {
@@ -26,11 +26,13 @@ public class CollectionChoiceExercises {
         // unique logged in usernames where adding a username that's already
         // present is ignored and checking if a user is in the collection can
         // be done very quickly.
+        HashSet<String> loggedInUsers;
     }
 
     public void MakeEmployeeAccessCodesCollection() {
         // Create an appropriate collection variable for holding a set of
         // key-value pairs where they key is an employee name (String) and the
         // value is an employee access code (String.)
+        HashMap<String, String> employeeAccessCodes;
     }
 }
